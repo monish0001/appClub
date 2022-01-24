66d7520c4e9de5e7058493bfa8cc39cf26786224
monish0001 <mohdmonishksg@gmail.com>
2022-01-25 01:18:03 +0530

    till day25

---
+++ day25/1.cpp
@@ -0,0 +1,55 @@
+class Solution {
+public:
+    int lengthOfLastWord(string s) {
+        s=' '+s;
+        int count =0;
+        int lastElement = s.size() - 1;
+        while (lastElement>= 0 && s[lastElement] == ' ')
+                     --lastElement;
+        s=s.substr(0,lastElement + 1);
+        int i=s.size()-1;
+        while(s[i]!=' '){
+            count++;
+            i--;
+        }
+        return count++;
+    }
+};
+
+/*
+58. Length of Last Word
+Easy
+
+Given a string s consisting of some words separated by some number of spaces, return the length of the last word in the string.
+
+A word is a maximal substring consisting of non-space characters only.
+
+ 
+
+Example 1:
+
+Input: s = "Hello World"
+Output: 5
+Explanation: The last word is "World" with length 5.
+
+Example 2:
+
+Input: s = "   fly me   to   the moon  "
+Output: 4
+Explanation: The last word is "moon" with length 4.
+
+Example 3:
+
+Input: s = "luffy is still joyboy"
+Output: 6
+Explanation: The last word is "joyboy" with length 6.
+
+ 
+
+Constraints:
+
+    1 <= s.length <= 104
+    s consists of only English letters and spaces ' '.
+    There will be at least one word in s.
+
+*/
